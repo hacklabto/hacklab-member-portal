f4f685acc43b4f80e973976995ad7c15b91aa65e
root <root@hacklab.to>
2015-10-23 14:18:20 -0400

    Added log messages to the member profile page.

--- routes/members.rb
+++ routes/members.rb
@@ -17,6 +17,7 @@ module Sinatra
             halt 403 if @member_id != params['id'].to_i && !is_ops?
             @member_info = @db[:members].where(:id => params['id']).first
             @waivers = @db[:waivers].select(:id, :name, (@db[:waiver_signatures].select(:id).where(:member_id => params[:id])).as("signed") )
+            @log_entries = @db[:log_entries].join(:log_entry_types, :log_entry_types__id => :type_id).where(:member_id => params[:id])
             haml :member
           end
         end
